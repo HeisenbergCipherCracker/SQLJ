9a10e5b1625d6f227a6b340dd5aaa80986e03d2b
alimirmohammad <ali@13>
2023-10-15 15:37:55 -0400

    Binary injection #1

--- Database/newdatabase.py
+++ Database/newdatabase.py
@@ -4,4 +4,9 @@ import authbypass_inj
 import sqlite3
 
 async def create_database_and_tables():
-    con = sqlite3.connect("Captures")
+    con = sqlite3.connect("Captures.db")
+    cur = con.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS payloads (id INTEGER PRIMARY KEY AUTOINCREMENT, payload TEXT)")
+    con.commit()
+    con.close()
+    
