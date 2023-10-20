419c4da953adebc570c47dd66ac249f233cff8e8
alimirmohammad <ali@13>
2023-10-20 11:58:02 -0400

    Memory error fixed

--- main/main.py
+++ main/main.py
@@ -151,7 +151,19 @@ else:
             
         except KeyboardInterrupt:
             continue
-        # except MemoryError:
-        #     res = Memory_handling()
-        #     await res
+        except MemoryError:
+            import psutil
+            # Get the system memory information
+            memory = psutil.virtual_memory()
+
+            # Calculate the threshold for 80% memory usage
+            threshold = memory.total * 0.9
+
+            # Check if the used memory is greater than the threshold
+            Err =  memory.used <= threshold
+            while not Err:
+                memory = psutil.virtual_memory()
+                Err = memory.used <= threshold
+                print("Please Release you RAM space to continue the application")
+                await asyncio.sleep(5)
     
