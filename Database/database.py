6f716acb43f831433be5d38821cc1c5835865161
alimirmohammad <ali@13>
2023-10-22 14:10:57 -0400

    Update for pre-release 1.0

--- Database/database.py
+++ Database/database.py
@@ -1,61 +1,15 @@
-import sqlite3
 import sys
-sys.path.append(r"D:\SQLjj\SQLJ\payloads")
-sys.path.append(r"D:\SQLjj\SQLJ\exploit")
-from payloads import main, auth_bypass_list_ls, generic_error_ls, generic_sql_inj_ls, generic_union_ls
-import asyncio
-from exploit.authbypass_inj import capturesAUTHBYPASS
-from exploit.Error_based_injection import capturesERRBASED
-from exploit.genericSQL import generic_capture
-from exploit.Timebasedinj import time_based_injection_capture
-from exploit.Unionselect import UNIOn_capture
+import sys
+sys.path.append('D:/SQLjj/SQLJ/exploit')
+import authbypass_inj
+from authbypass_inj import auth_SQL_inj
+import sys
+import os
+import sqlite3
 
-###############################################################################
-extraction = []
-
-lists_extracted = []
-
-
-###############################################################################3
-
-
-
-async def database_for_payloads():
-    await main()
-    con = sqlite3.connect("payLoadsS.db")
+def Create_data_base_for_captures():
+    con = sqlite3.connect("Captures.db")
     cur = con.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS payload (column1 datatype1, column2 datatype2)")
-
-    # Insert the lists into the table
-    payload_data = [(value,) for value in auth_bypass_list_ls] + [(value,) for value in generic_error_ls] + [(value,) for value in generic_sql_inj_ls] + [(value,) for value in generic_union_ls]
-    cur.executemany("INSERT INTO payload (column1) VALUES (?)", payload_data)
-
-    # Commit the changes
+    cur.execute("CREATE TABLE IF NOT EXISTS Datas (id INTEGER PRIMARY KEY AUTOINCREMENT, Data TEXT)")
     con.commit()
-
-    # Close the connection
     con.close()
-
-async def retrieve_data():
-    await database_for_payloads()
-    con = sqlite3.connect("payLoadsS.db")
-    cur = con.cursor()
-    cur.execute("SELECT * FROM payload")
-    rows = cur.fetchall()
-    for row in rows:
-        global extraction
-        extraction.append(list(row))
-    
-    return extraction
-
-async def main_prog():
-    await database_for_payloads()
-    await retrieve_data()
-
-    # asyncio.run(retrieve_data())
-#     print(extraction)
-# asyncio.run(main_prog())
-
-
-async def data_base_for_captures():
-    pass
