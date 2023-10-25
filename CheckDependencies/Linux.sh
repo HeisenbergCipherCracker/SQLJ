6a4eda66a9547de15a3858b115fc1e51cfdeaf98
alimirmohammad <ali@13>
2023-10-25 16:22:46 -0400

    Added Dependencies for Linux

---
+++ CheckDependencies/Linux.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install Colorama using pip
+pip install colorama
+pip install datetime
+pip install time
+pip install glob
+pip install psutil
+
+# Your other shell script commands here
