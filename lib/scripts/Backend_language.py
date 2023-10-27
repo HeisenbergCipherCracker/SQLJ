96a8f63ff86a268523b1a01ae8a3728788876c68
alimirmohammad <ali@13>
2023-10-27 17:26:18 -0400

    Update 1.21

--- lib/scripts/Backend_language.py
+++ lib/scripts/Backend_language.py
@@ -1,5 +1,5 @@
 import requests
-from colorama import Fore,init
+from colorama import Fore,init,Style
 import asyncio
 
 async def find_backend_language(Link):
@@ -14,9 +14,9 @@ async def find_backend_language(Link):
     if "Server" in response.headers:
         server_header = response.headers["Server"]
         # print(Fore.GREEN+"[INFO] looks like the Backend language is with", Fore.RESET+Fore.RED+server_header)
-        print(f"{Fore.RESET}Looks like the backend language of {Fore.RESET}{Fore.RED}{Link} is with {Fore.RESET}{Fore.GREEN}{server_header}.{Fore.RESET}Do you want to continue?(y/q)")
+        print(f"{Fore.RESET}{Style.BRIGHT}**[INFO]Looks like the backend language of {Fore.RESET}{Fore.RED}{Link} is with {Fore.RESET}{Fore.GREEN}{server_header}.{Fore.RESET}Do you want to continue?(y/q)**")
     else:
         print(f"Could not found any info related to the backend on the website {Link}.")
         
-# print(asyncio.run(find_backend_language("")))
+asyncio.run(find_backend_language("http://testfire.net/login.jsp"))
 # print(type(ask))
