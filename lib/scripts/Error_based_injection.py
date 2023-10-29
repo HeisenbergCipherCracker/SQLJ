7d477c1634bd745c3fbfbce26f77cb75a764c4cf
alimirmohammad <ali@13>
2023-10-29 15:46:47 -0400

    logs #5

--- lib/scripts/Error_based_injection.py
+++ lib/scripts/Error_based_injection.py
@@ -9,6 +9,9 @@ import traceback
 from bs4 import BeautifulSoup
 import sqlite3
 # from database import create_database_for_Captures
+import logging
+
+logging.basicConfig(filename="SQLJ.log",level=logging.DEBUG)
 
 init()
 
@@ -51,6 +54,7 @@ async def Error_based_inj(urls):
             req = requests.get(url=urls,verify=False)
             if req.status_code == 200:
                 ask = input(f"[{datetime.now()}]"+Fore.GREEN + f"Looks like the host is up with the url: {urls}\n Do you want to send the payload to the website? ")
+                logging.info(f"The host is up:{urls},Time:{datetime.now()},attack:{attack_type}")
 
                 if ask.lower() == "y":
                     for line in sorted_payload.split("\n"):
@@ -64,10 +68,12 @@ async def Error_based_inj(urls):
                         # print(line) #*  We may need that 
                         ack = requests.post(url=urls, data=params,verify=False)
                         print(f"[{datetime.now()}]","|Current payload: |", line,"|with status code|:",ack.status_code,"|Attack:|",attack_type)
+                        logging.info(f"sending payloads to the target:{urls},Time:{datetime.now()},attack:{attack_type}")
                         print(f"[{datetime.now()}]",Fore.GREEN + str(ack.status_code))
                         await asyncio.sleep(5)
                         if "error" in ack.text:
                             print(f"[{datetime.now()}]",Fore.RED + "|Vulnerability found In the error based attack|:","error" in ack.text,"|Attack:|",attack_type)
+                            logging.info(f"Could find the injectable parameter(error) in the target:{urls},Time:{datetime.now()},attack:{attack_type}")
                             
                         vuln = re.findall(pattern=pattern,string=ack.text,flags=re.IGNORECASE)
                         htmlVULN = re.findall(pattern=htmlpattern,string=ack.text,flags=re.IGNORECASE)
@@ -77,30 +83,36 @@ async def Error_based_inj(urls):
                         
                         if htmlVULN:
                             print(f"[{datetime.now()}]",Fore.RED + "|Vulnerability found Status|:", ack.text,"with the count of:",len(htmlVULN)if len(str(htmlVULN)) != 0 else "|Nothing found|","|Attack:|",attack_type)
+                            logging.info(f"Could find injectable area in the html doc,target:{urls},time:{datetime.now()},attack:{attack_type}")
                             await asyncio.sleep(3)
                         
                         word = "id" in req.text
                         errword = "error" in req.text
                         if word:
                             print(f"[{datetime.now()}]",Fore.GREEN + "|Vulnerability found Status In error based attack|:", "Founded!!!" if word is True else "DID NOT found.","|Attack:|",attack_type)
+                            logging.info(f"Could find the injectable parameter(id) in the target:{urls},Time:{datetime.now()},attack:{attack_type}")
                             await asyncio.sleep(3)
                         
                         if errword:
                             print(f"[{datetime.now()}]",Fore.RED + "|Vulnerability found|:", "Founded!!!" if errword is True else "Did not Found","|Attack:|",attack_type)
+                            logging.info(f"Could find the injectable parameter(error) in the target:{urls},Time:{datetime.now()},attack:{attack_type}")
                             await asyncio.sleep(3)
                             
                         
                             
                     if ack.status_code == 302:
                         print(f"[{datetime.now()}]","Could find the injectable area on the website:",line,"|Attack:|",attack_type)
+                        logging.info(f"Could find the injectable area on the website:{urls},Time:{datetime.now()},attack:{attack_type}")
                         done = True
                         
                     if "Admin" in vuln or "admin" in vuln or "Admin" in ack.text or "admin" in ack.text or "Admin" in htmlVULN or "admin" in htmlVULN:
                         print(f"[{datetime.now()}]",Fore.GREEN+"[INFO]Could connect to the website but did found injectable area on the website.","|Attack:|",attack_type)
+                        logging.info(f"Could connect to the website but did found injectable area on the website:{urls},Time:{datetime.now()},attack:{attack_type}")
                         
                         
             else:
                 print(f"[{datetime.now()}]",Fore.RED + "Looks like the host is down with the ip address: {socket.gethostbyname(url)}","|Attack:|",attack_type)
+                logging.error(f"Looks like the host is down with the ip address: {socket.gethostbyname(url)}")
         
         conn = sqlite3.connect("SQLJresult.db")
         cur = conn.cursor()
@@ -121,15 +133,19 @@ async def Error_based_inj(urls):
         
     except ConnectionAbortedError as e:
         print(f"[{datetime.now()}]",Fore.RED+"[ERROR]Error:",e,"|Attack:|",attack_type)
+        logging.error(f"Connection aborted:{urls},attack:{attack_type},Time:{datetime.now()}")
         
     except ConnectionError as e:
         print(f"[{datetime.now()}]",Fore.RED+"[ERROR]Error:",e,"|Attack:|",attack_type)
+        logging.error(f"Connection error:{urls},attack:{attack_type},Time:{datetime.now()}")
         
     except ConnectionRefusedError as e:
         print(f"[{datetime.now()}]",Fore.RED+"[ERROR]Error:",e)
+        logging.error(f"Connection refused:{urls},attack:{attack_type},Time:{datetime.now()}")
 
     except ConnectionResetError as e:
         print(f"[{datetime.now()}]",Fore.RED+"[ERROR]Error:",e)
+        logging.error(f"Connection reset:{urls},attack:{attack_type},Time:{datetime.now()}")
         
     except KeyboardInterrupt:
         # print(f"[{datetime.now()}]","Exiting...")
