78542704198ce9ada71379c1a601b261059f05cc
alimirmohammad <ali@13>
2023-11-01 12:21:41 -0400

    payloads setup

---
+++ Core/Socketapi/payloadsetup.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.append('D:\SQLjj\SQLJ\lib\scripts')
+from colorama import Fore
+from colorama import Style
+from payload4authpypass import payload4auth_bypass
+from payload4errorbased import Error_based4payload
+from payload4generic import payload4generic
+from payload4unionselect import payload4unionselect
+from time4timebased import timebasedpayload
