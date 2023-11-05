9f81ea015d78017c0ddef47df2bafab509eaccb8
alimirmohammad <ali@13>
2023-11-05 01:52:12 -0500

    LANG #2

--- lib/scripts/Backend_language.py
+++ lib/scripts/Backend_language.py
@@ -27,7 +27,7 @@ async def Find_back_end_auto(Link):
     if "Server" in response.headers:
         server_header = response.headers["Server"]
         # print(Fore.GREEN+"[INFO] looks like the Backend language is with", Fore.RESET+Fore.RED+server_header)
-        return f"{Fore.RESET}{Style.BRIGHT}**[INFO]Looks like the backend language of {Fore.RESET}{Fore.RED}{Link} is with {Fore.RESET}{Fore.GREEN}{server_header}.{Fore.RESET}Do you want to continue?(y/q)**"
+        return f"{Fore.RESET}{Style.BRIGHT}**[INFO]Looks like the server of {Link} is running on: {Fore.RESET}{Fore.RED}{Link} is with {Fore.RESET}{Fore.GREEN}{server_header}.{Fore.RESET}Do you want to continue?(y/q)**"
     else:
         return f"Could not found any info related to the backend on the website {Link}."
     
