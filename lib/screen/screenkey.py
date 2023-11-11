df501a67a362753505cd6e7fcf7b22c2e00eebcd
alimirmohammad <ali@13>
2023-11-10 22:47:01 -0500

    Key #15

--- lib/screen/screenkey.py
+++ lib/screen/screenkey.py
@@ -3,6 +3,12 @@ import os
 
 
 class Key_Handler:
+    
+    @staticmethod
+    def F1_btn(key):
+        if key == keyboard.Key.f1:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            return "F1"
     @staticmethod
     def F2_btn(key):
         if key == keyboard.Key.f2:
@@ -15,7 +21,72 @@ class Key_Handler:
             return "F3"   
     
     @staticmethod
-    def F4_btn(key) 
+    def F4_btn(key):
+        if key == keyboard.Key.f4:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            return "F4"
+    
+    @staticmethod
+    def F5_btn(key):
+        if key == keyboard.Key.f5:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            return "F5"
+    
+    @staticmethod
+    def F6_btn(key):
+        if key == keyboard.Key.f6:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            return "F6"
+    
+    @staticmethod
+    def F7_btn(key):
+        if key == keyboard.Key.f7:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            return "F7"
+    
+    @staticmethod
+    def F8_btn(key):
+        if key == keyboard.Key.f8:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            return "F8"
+    
+    @staticmethod
+    def F9_btn(key):
+        if key == keyboard.Key.f9:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            return "F9"
+        
+    @staticmethod
+    def F8_btn(key):
+        if key == keyboard.Key.f8:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            return "F8"
+    
+    @staticmethod
+    def F9_btn(key):
+        if key == keyboard.Key.f9:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            return "F9"
+    
+    @staticmethod
+    def F10_btn(key):
+        if key == keyboard.Key.f10:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            return "F10"
+        
+    @staticmethod
+    def F11_btn(key):
+        if key == keyboard.Key.f11:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            return "F11"
+    
+    @staticmethod
+    def F12_btn(key):
+        if key == keyboard.Key.f12:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            return "F12"
+    
+        
 
 
     def on_release(self, key):
