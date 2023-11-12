68a1b366a493436a0419285d1e3b8d0e737eabb7
Heisenberg <97875042+HeisenbergCipherCracker@users.noreply.github.com>
2023-11-12 23:32:34 +0000

    Minor update

--- lib/scripts/Auth_bypass_inj_with_HEADERS.py
+++ lib/scripts/Auth_bypass_inj_with_HEADERS.py
@@ -57,7 +57,6 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
 }
 
-###############################################
 
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
