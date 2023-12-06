52a65535c20a8af9128e53e34ec49a1cf78133e6
Heisenberg <97875042+HeisenbergCipherCracker@users.noreply.github.com>
2023-12-06 00:33:48 +0000

    Styles added to the attack type

--- lib/Attacktype/Attacks.py
+++ lib/Attacktype/Attacks.py
@@ -1,13 +1,24 @@
-class AttackTypes:
-    GENERIC_SQL_INJECTION = "GENERIC_SQL_INJECTION"
-    AUTH_BYPASS_SQL_INJECTION = "AUTH_BYPASS_SQL_INJECTION"
-    TIME_BASED_SQL_INJECTION = "TIME_BASED_SQL_INJECTION"
-    ERROR_BASED_SQL_INJECTION = "ERROR_BASED_SQL_INJECTION"
-    PRIVILLAGE_ESCALATION = "PRIVILLAGE_ESCALATION"
-    NO_SQL = "NO_SQL"
-    MY_SQL = "MYSQL"
-    ORACLE = "ORACLE"
-    INVALIDHTTP_REQ = "INVALID_HTTP_REQ"
+from enum import Enum
+from colorama import Fore,init,Style
+
+init()
+
+class AttackType(Enum):
+    GENERIC_SQL_INJECTION = f"{Fore.RESET}{Fore.WHITE}{Style.RESET_ALL}{Style.BRIGHT}generic sql injection (payloads)"
+    AUTH_BYPASS_SQL_INJECTION = "authentication bypass sql injection (payloads)"
+    TIME_BASED_SQL_INJECTION = "time based sql injection (payloads)"
+    ERROR_BASED_SQL_INJECTION = "error based sql injection (payloads)"
+    PRIVILLAGE_ESCALATION = "privilege escalation (payloads)"
+    NO_SQL = "no sql(payloads)"
+    MY_SQL = "MYSQL (payloads)"
+    ORACLE = "ORACLE injection (payloads)"
+    INVALIDHTTP_REQ = "invalid http (payloads)"
+
+a = AttackType.GENERIC_SQL_INJECTION
+
+print(f"using:{a.value}")
+
+
 
 
 
