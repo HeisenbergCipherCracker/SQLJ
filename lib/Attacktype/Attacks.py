79559abd50a1a9dc66076ef3da8135c98af5f170
Heisenberg <97875042+HeisenbergCipherCracker@users.noreply.github.com>
2023-12-06 00:35:44 +0000

    Some Styles added to the attack types

--- lib/Attacktype/Attacks.py
+++ lib/Attacktype/Attacks.py
@@ -5,18 +5,18 @@ init()
 
 class AttackType(Enum):
     GENERIC_SQL_INJECTION = f"{Fore.RESET}{Fore.WHITE}{Style.RESET_ALL}{Style.BRIGHT}generic sql injection (payloads)"
-    AUTH_BYPASS_SQL_INJECTION = "authentication bypass sql injection (payloads)"
-    TIME_BASED_SQL_INJECTION = "time based sql injection (payloads)"
-    ERROR_BASED_SQL_INJECTION = "error based sql injection (payloads)"
-    PRIVILLAGE_ESCALATION = "privilege escalation (payloads)"
-    NO_SQL = "no sql(payloads)"
-    MY_SQL = "MYSQL (payloads)"
-    ORACLE = "ORACLE injection (payloads)"
-    INVALIDHTTP_REQ = "invalid http (payloads)"
+    AUTH_BYPASS_SQL_INJECTION = f"{Fore.RESET}{Fore.WHITE}{Style.RESET_ALL}{Style.BRIGHT}authentication bypass sql injection (payloads)"
+    TIME_BASED_SQL_INJECTION = f"{Fore.RESET}{Fore.WHITE}{Style.RESET_ALL}{Style.BRIGHT}time based sql injection (payloads)"
+    ERROR_BASED_SQL_INJECTION = f"{Fore.RESET}{Fore.WHITE}{Style.RESET_ALL}{Style.BRIGHT}error based sql injection (payloads)"
+    PRIVILLAGE_ESCALATION = f"{Fore.RESET}{Fore.WHITE}{Style.RESET_ALL}{Style.BRIGHT}privilege escalation (payloads)"
+    NO_SQL = f"{Fore.RESET}{Fore.WHITE}{Style.RESET_ALL}{Style.BRIGHT}no sql(payloads)"
+    MY_SQL = f"{Fore.RESET}{Fore.WHITE}{Style.RESET_ALL}{Style.BRIGHT}MYSQL (payloads)"
+    ORACLE = f"{Fore.RESET}{Fore.WHITE}{Style.RESET_ALL}{Style.BRIGHT}ORACLE injection (payloads)"
+    INVALIDHTTP_REQ = f"{Fore.RESET}{Fore.WHITE}{Style.RESET_ALL}{Style.BRIGHT}invalid http (payloads)"
 
-a = AttackType.GENERIC_SQL_INJECTION
+# a = AttackType.GENERIC_SQL_INJECTION
 
-print(f"using:{a.value}")
+# print(f"using:{a.value}")
 
 
 
