8e86cd3375005d2f8202a4e332715ed2e98b0044
HeisenbergCipherCracker <alimirmohammad.1386@gmail.com>
2023-12-09 13:36:02 -0500

    Changes for patch

--- lib/sound/sound.py
+++ lib/sound/sound.py
@@ -1,20 +1,12 @@
 import os
-from pydub import AudioSegment
-import simpleaudio as sa
 
-def play_wav(filename):
-    # Get the current path and construct the file_path
-    current_directory = os.path.dirname(os.path.abspath(__file__))
-    file_path = os.path.join(current_directory, "lib", "sound", filename)
 
-    # Load the WAV file using pydub
-    sound = AudioSegment.from_file(file_path, format="wav")
+class Playsound:
+    @staticmethod
+    def playsound_normal(fname):
+        current_directory = os.getcwd()
+        path = os.path.join(current_directory, fname)
+        os.system(f"afplay {fname}")
 
-    # Convert to bytes and play using simpleaudio
-    wave_obj = sa.WaveObject(sa.samples_to_array(sound.raw_data), sound.channels, sound.sample_width, sound.frame_rate)
-    play_obj = wave_obj.play()
-    play_obj.wait_done()
 
-# Example usage
-filename = "BEEP.wav"
-play_wav(filename)
+# Playsound.playsound_normal("beep.wav")
