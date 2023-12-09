9903a10dc9c5ab1276d4e0b01f8dddc593e2f1eb
HeisenbergCipherCracker <alimirmohammad.1386@gmail.com>
2023-12-08 19:59:26 -0500

    logger updated in host name oracle attack

--- lib/Attacktype/Attacks.py
+++ lib/Attacktype/Attacks.py
@@ -27,6 +27,7 @@ class OracleAttacks(Enum):
     DATABASE_COLUMN_LIST =  f"{Fore.RESET}{Fore.WHITE}{Style.RESET_ALL}{Style.BRIGHT}database column list (payloads)"
     DATABASE_LIST = f"{Fore.RESET}{Fore.WHITE}{Style.RESET_ALL}{Style.BRIGHT}database list (payloads)"
     DATABASE_NAME = f"{Fore.RESET}{Fore.WHITE}{Style.RESET_ALL}{Style.BRIGHT}database name (payloads)"
+    HOST_NAME_INJECTION = f"{Fore.RESET}{Fore.WHITE}{Style.RESET_ALL}{Style.BRIGHT}host name  (payloads)"
 
 
 
