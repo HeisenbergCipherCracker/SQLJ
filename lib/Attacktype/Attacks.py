6b616e20eae97f3e1a2d87b8451926f34a08a677
HeisenbergCipherCracker <alimirmohammad.1386@gmail.com>
2023-12-08 20:09:55 -0500

    minor patch

--- lib/Attacktype/Attacks.py
+++ lib/Attacktype/Attacks.py
@@ -32,6 +32,7 @@ class OracleAttacks(Enum):
 
 class ErrorBasedSQl(Enum):
     INVALID_HTTP_REQ = f"{Fore.RESET}{Fore.WHITE}{Style.RESET_ALL}{Style.BRIGHT}invalid http request (payloads)"
+    BASIC_SQL_Error_Based = f"{Fore.RESET}{Fore.WHITE}{Style.RESET_ALL}{Style.BRIGHT}basic sql (payloads)"
 
 
 
