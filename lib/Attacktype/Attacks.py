ff18fcf31770d9bca55fb22aa97f5db85a7320ba
HeisenbergCipherCracker <alimirmohammad.1386@gmail.com>
2023-12-08 20:14:37 -0500

    Attacks

--- lib/Attacktype/Attacks.py
+++ lib/Attacktype/Attacks.py
@@ -34,6 +34,7 @@ class ErrorBasedSQl(Enum):
     INVALID_HTTP_REQ = f"{Fore.RESET}{Fore.WHITE}{Style.RESET_ALL}{Style.BRIGHT}invalid http request (payloads)"
     BASIC_SQL_Error_Based = f"{Fore.RESET}{Fore.WHITE}{Style.RESET_ALL}{Style.BRIGHT}basic sql (payloads)"
     MYSQL_EXTRACT_VALUE = f"{Fore.RESET}{Fore.WHITE}{Style.RESET_ALL}{Style.BRIGHT}mysql extract value(payloads)"
+    MYSQL_NAME_CONST = f"{Fore.RESET}{Fore.WHITE}{Style.RESET_ALL}{Style.BRIGHT}name const(payloads)"
 
 
 
