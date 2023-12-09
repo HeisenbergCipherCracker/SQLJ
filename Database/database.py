07367add985b0c6cb878aaec1f1fbca4261e1a8e
HeisenbergCipherCracker <alimirmohammad.1386@gmail.com>
2023-12-09 10:42:29 -0500

    Some changes made

--- Database/database.py
+++ Database/database.py
@@ -1,15 +1,1 @@
-import sys
-import sys
-sys.path.append('D:/SQLjj/SQLJ/exploit')
-import authbypass_inj
-from authbypass_inj import auth_SQL_inj
-import sys
-import os
-import sqlite3
-
-def Create_data_base_for_captures():
-    con = sqlite3.connect("Captures.db")
-    cur = con.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS Datas (id INTEGER PRIMARY KEY AUTOINCREMENT, Data TEXT)")
-    con.commit()
-    con.close()
+pass
