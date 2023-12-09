d7f7039e65701fe5cecc7e425dcb2d1238dd1d5e
HeisenbergCipherCracker <alimirmohammad.1386@gmail.com>
2023-12-09 17:48:01 -0500

    re pattern for ip address

--- lib/regelexpression/ippatterns.py
+++ lib/regelexpression/ippatterns.py
@@ -11,6 +11,14 @@ def find_ipv4type_private(ip:str)->bool:
         return True,"this ip is private"
     
     return False,"this ip is not private"
+
+
+def find_ipv4_public_is(ip:str)->bool:
+    pattern = re.compile(r"^(?:192\.168|10|172\.(?:1[6-9]|2[0-9]|3[0-1]))\.\d{1,3}\.\d{1,3}$")
+    if pattern.match(ip):
+        return False,"ip is public"
     
-print(find_ipv4type_private("192.168.1.1"))
+    return True,"ip is not public"
+    
+print(find_ipv4_public_is("172.217.1.4"))
     
