ddc1922eee6479fd4ea15ce56561145c4bf0ce75
HeisenbergCipherCracker <alimirmohammad.1386@gmail.com>
2023-12-12 21:11:59 -0500

    general naming

---
+++ INFO/common.py
@@ -0,0 +1,16 @@
+tables = [
+    "users",
+    "customers",
+    "products",
+    "orders",
+    "invoices",
+]
+
+columns = [
+    "id",
+    "name",
+    "email",
+    "phone",
+    "create_at",
+    "updated_at"
+]
