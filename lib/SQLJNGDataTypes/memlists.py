9b2cebffaf198a1c789fa5bfc88743698ca48d64
HeisenbergCipherCracker <alimirmohammad.1386@gmail.com>
2023-12-20 19:11:21 -0500

    sqlite and meme lists added

---
+++ lib/SQLJNGDataTypes/memlists.py
@@ -0,0 +1,26 @@
+class Memelist(list):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+    def get_list_as_generator_obj(self,ls:list):
+        for i in ls:
+            yield i
+    
+    def get_list_as_generator_exp(self,ls:list):
+        gen_expr = (x for x in ls)
+        return gen_expr
+    
+    def __getitem__(self, key):
+        return self.get_list_as_generator_obj(super().__getitem__(key))
+    
+my_list = Memelist([1, 2, 3, 4, 5])
+
+# Use the get_list_as_generator_obj method
+gen_obj = my_list.get_list_as_generator_obj(my_list)
+for item in gen_obj:
+    print(item)
+
+# Use the get_list_as_generator_exp method
+gen_exp = my_list.get_list_as_generator_exp(my_list)
+for item in gen_exp:
+    print(item)
